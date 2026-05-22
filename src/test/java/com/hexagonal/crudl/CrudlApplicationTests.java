109d06c37eefe342bdfc5132a659fcc0a3fec377
Sergio Vargas <sergiolvargas95@gmail.com>
2026-05-21 22:32:55 -0500

    initial commit

---
+++ src/test/java/com/hexagonal/crudl/CrudlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.crudl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
